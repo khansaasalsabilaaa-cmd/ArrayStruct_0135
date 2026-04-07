8152c393174f802827469f5403222fee1b6c2f29
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-04-07 17:31:22 +0700

    menambahkan perintah

--- array.cpp
+++ array.cpp
@@ -18,4 +18,14 @@ int main()
     cout << " Isikan data pertama : ";
     cin >> datamu [0];
     cout << "Isikan data kedua : ";
+    cin >> datamu[1];
+    cout << "Isikan data ketiga : ";
+    cin >> datamu[2];
+    cout << "Isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "Isikan data kelima : ";
+    cin >> datamu[4];
+    cout << endl;
+    
 }
+
