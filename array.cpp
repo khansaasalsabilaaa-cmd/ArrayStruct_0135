e382c178d285fc00a076ff6633087930ac3faa11
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-04-07 17:19:45 +0700

    menambahakan data kedua

--- array.cpp
+++ array.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     //deklarasi array dengan inisialisasi
     int dataku [5] = {10,20,30,40,50};
     //deklarasi tanpa inisialisasi
     int datamu [5];
+
+    //cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : " << dataku[1]<< endl;
+    //mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah diubah : " << dataku[1] << endl;
 }
