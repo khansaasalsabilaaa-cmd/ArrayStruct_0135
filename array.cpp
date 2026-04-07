6683c88654dbd15c08e44ede46d77ddd2cb8913c
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-04-07 17:15:22 +0700

    menambahkan deklarasi array

--- array.cpp
+++ array.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    //deklarasi array dengan inisialisasi
+    int dataku [5] = {10,20,30,40,50};
+    //deklarasi tanpa inisialisasi
+    int datamu [5];
+}
